3db19c778209776d092b0868326766ef7d5b631f
alijbarghouth <alijbarghouth@gmail.com>
2022-09-20 18:04:52 +0300

    first commit

---
+++ Country.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    public class Country
+    {
+        
+        public String Name
+        {
+            get;
+        }
+        public String Code
+        {
+            get;
+        }
+        public String Region
+        {
+            get;
+        }
+        public int Population
+        {
+            get;
+        }
+        public Country(String Name, string Code,String Region,int Population)
+        {
+            this.Name = Name;
+            this.Code = Code;
+            this.Region = Region;
+            this.Population = Population;
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Dictionaries;
+using System.Diagnostics.Metrics;
+
+namespace Dictinaries
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Country india = new Country("INDIA", "in", "20h", 20000000); 
+            Country egipt = new Country("Egipt", "Eg", "20p", 10000000);
+            var dictionary = new Dictionary<String, Country> { {india.Code,india },{ egipt.Code,egipt} };
+            Country Country = dictionary[india.Code];
+            Console.WriteLine(Country.Name);
+            Console.WriteLine(dictionary.Count);
+            bool exist =  dictionary.TryGetValue("in",out Country country);
+            dictionary.Remove(india.Code.ToString());
+            if (exist)
+            {
+                Console.WriteLine(country.Name);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            foreach(var b in dictionary)
+            {
+                Console.WriteLine(b.Value.Name);
+            }
+        }
+    }
+}
